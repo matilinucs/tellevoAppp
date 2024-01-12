ceb0115d83b96724390473af3f3b0f0dee4f0811
Hyrum Carrasco Inzunza <83139172+Hyro25@users.noreply.github.com>
2024-01-12 10:03:50 -0300

    Update crear-viaje.page.ts

--- src/app/pages/crear-viaje/crear-viaje.page.ts
+++ src/app/pages/crear-viaje/crear-viaje.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular'; // Agrega esta línea
 
 @Component({
   selector: 'app-crear-viaje',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearViajePage implements OnInit {
 
-  constructor() { }
+  destino: string = ''; // Asigna un valor inicial o marca como opcional añadiendo '?'
+  fecha: string = ''; // Asigna un valor inicial o marca como opcional añadiendo '?'
+  asientosDisponibles: number = 1; // Asigna un valor inicial o marca como opcional añadiendo '?'
+  minDate: string;
+
+  constructor(private toastController: ToastController, private router: Router) { 
+    // Configurar la fecha mínima como hoy
+    const today = new Date();
+    this.minDate = today.toISOString();
+
+    // Inicializar la propiedad fecha con la fecha mínima
+    this.fecha = this.minDate;
+  }
 
   ngOnInit() {
   }
 
+  
+  
+  realizarCompra() {
+    // Lógica de compra aquí
+    // Puedes mostrar un mensaje de éxito utilizando una alerta de Ionic
+    alert('¡Compra Exitosa!');
+      // Navegar a la página "terminado"
+  this.router.navigate(['/terminado']);
+
+  }
 }
